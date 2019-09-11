71c99c3e93653c655cbed0116b856ca7d24fedac
William Quesada <wquesadahidalgo@gmail.com>
2019-09-10 18:29:07 -0600

    Clase 48

---
+++ bases/views.py
@@ -0,0 +1,10 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views import generic
+
+
+class Home(LoginRequiredMixin, generic.TemplateView):
+    template_name = 'bases/home.html'
+    login_url = '/admin'
+
+
+
