10d386d83022d25ce064696df48e67b135b4e0a8
William Quesada <wquesadahidalgo@gmail.com>
2019-09-12 17:19:12 -0600

    Clase 61

--- bases/models.py
+++ bases/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class ClassModelo(models.Model):
+    estado = models.BooleanField(default=True)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    fecha_modificacion = models.DateTimeField(auto_now=True)
+    usuario_creado = models.ForeignKey(User, on_delete=models.CASCADE)
+    usuario_modificacion = models.IntegerField(blank=True, null=True)
+
+    class Meta:
+        abstract = True
+
+
+
--- bases/urls.py
+++ bases/urls.py
@@ -9,3 +9,4 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='bases/login.html'), name="login"),
     path('logout/', auth_views.LogoutView.as_view(template_name='bases/login.html'), name="logout"),
 ]
+
